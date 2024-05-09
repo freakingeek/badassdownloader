1a0168ee4b3cf86d40197c633fa36dcb70a3f949
Yasin Silavi <ys.silavi@gmail.com>
2024-05-09 20:18:21 +0330

    feat: add tiktok uploader logic

--- src/features/bot/index.ts
+++ src/features/bot/index.ts
@@ -1,11 +1,14 @@
 import { Input, Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
+import tiktokDownloader from "../platforms/tiktok.js";
 import { platformFinder } from "../../utils/platforms.js";
 import { Animations, Platforms } from "../../configs/enums.js";
 
 const bot = new Telegraf(process.env.BOT_TOKEN!);
 
 bot.start((ctx) => ctx.reply("Just pass the link"));
+
+const memo: Record<string, string> = {};
 
 bot.on(message("text"), async (ctx) => {
   const message = ctx.update.message.text;
@@ -20,7 +23,28 @@ bot.on(message("text"), async (ctx) => {
     return;
   }
 
-  await ctx.reply("Yep were working fine!");
+  await ctx.reply("Just a sec ..");
+
+  if (message in memo) {
+    await ctx.persistentChatAction("upload_video", async () => {
+      await ctx.replyWithVideo(Input.fromFileId(memo[message]), {
+        caption: "@badassdownloader",
+      });
+    });
+
+    return;
+  }
+
+  const buffer = await tiktokDownloader(message);
+
+  ctx.reply("Just little more ..");
+  await ctx.persistentChatAction("upload_video", async () => {
+    const file = await ctx.replyWithVideo(Input.fromBuffer(buffer), {
+      caption: "@badassdownloader",
+    });
+
+    memo[message] = file.video.file_id;
+  });
 });
 
 export default bot;
